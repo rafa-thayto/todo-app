69ac3de1bc45c0ab7e5124be852d815c63af5888
Rafael Thayto <rafa.thayto@gmail.com>
2017-11-17 21:55:43 -0200

    Todo list is working

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -17,6 +17,8 @@ export default class Todo extends Component {
         this.handleAdd = this.handleAdd.bind(this);
 
         this.handleRemove = this.handleRemove.bind(this);
+        this.handleMarkAsDone = this.handleMarkAsDone.bind(this);
+        this.handleMarkAsPending = this.handleMarkAsPending.bind(this);
         
         this.refresh();
     }
@@ -41,14 +43,28 @@ export default class Todo extends Component {
             .then(resp => this.refresh());
     }
     
+    handleMarkAsDone(todo) {
+        axios.put(`${URL}/${todo._id}`, { ...todo, done: true})
+            .then(resp => this.refresh());
+    }
+    
+    handleMarkAsPending(todo) {
+        axios.put(`${URL}/${todo._id}`, { ...todo, done: false })
+            .then(resp => this.refresh());
+    }
+    
     render() {
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro' />
-                <TodoForm description={ this.state.description }
+                <TodoForm 
+                    description={ this.state.description }
                     handleAdd={ this.handleAdd }
                     handleChange={ this.handleChange } />
-                <TodoList list={ this.state.list }
+                <TodoList 
+                    list={ this.state.list }
+                    handleMarkAsDone={ this.handleMarkAsDone }
+                    handleMarkAsPending={ this.handleMarkAsPending }
                     handleRemove={ this.handleRemove } />
             </div>
         )
